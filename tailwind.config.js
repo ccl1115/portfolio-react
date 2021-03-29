af3b837f2d1d33e664eb6aaad8903f6a6a378580
禹璐 <yulu@chemi.ren>
2021-03-29 17:16:26 +0800

    purge for jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const production = process.env.NODE_ENV;
 module.exports = {
-  purge: production ? ['./src/**/*.sass'] : [],
+  purge: production ? ['./src/**/*.sass', './src/**/*.jsx'] : [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
